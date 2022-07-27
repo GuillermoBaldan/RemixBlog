4972250380fbaa506c73a4a1de90886099570b75
GuillermoBaldan <guillermobaldan@gmail.com>
2022-07-27 22:39:25 +0200

    style added

--- app/root.jsx
+++ app/root.jsx
@@ -7,24 +7,42 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+import globalStyles from '~/styles/global.css'
+
 export const meta = () => ({
   charset: "utf-8",
   title: "New Remix App",
   viewport: "width=device-width,initial-scale=1",
 });
 
+function Layout () {
+return (
+  <>
+    <header>
+    <h1>RemixBlog ✨</h1>
+    <p>The place where you´ll find more than code</p>
+    </header>
+    <Outlet />
+    <footer>
+    ©Copyrigth 2022 BaldanCode. No Te lo copies!!
+    </footer>
+  </>
+)
+}
+
 export default function App() {
   return (
     <html lang="en">
       <head>
+        <title>RemixBlog</title>
         <Meta />
 
-        <Links />
+        <link rel='stylesheet' href={globalStyles} />
       </head>
 
       <body>
-        <Outlet />
-
+       
+      <Layout />
         <ScrollRestoration />
 
         <Scripts />
